c60c7fe7b2c1f17c80a0d18a81631e26b529f86d
Konjoko <seyedmehdialavi93@gmail.com>
2026-05-27 23:16:01 +0400

    add tajrobe tajer post and update sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,20 +1,21 @@
 import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
- return [
-   { url: "https://www.konjoko.com", lastModified: new Date() },
-   { url: "https://www.konjoko.com/blog", lastModified: new Date() },
-   { url: "https://www.konjoko.com/blog/kharid-omde-dubai-chin", lastModified: new Date() },
-   { url: "https://www.konjoko.com/blog/kharid-az-dubai", lastModified: new Date() },
-   { url: "https://www.konjoko.com/blog/pardakht-beinalmelli", lastModified: new Date() },
-   { url: "https://www.konjoko.com/blog/kharid-az-chin", lastModified: new Date() },
-   { url: "https://www.konjoko.com/blog/kharid-az-turkey", lastModified: new Date() },
-   { url: "https://www.konjoko.com/blog/havaleh-arzi", lastModified: new Date() },
-   { url: "https://www.konjoko.com/blog/kharid-az-amazon", lastModified: new Date() },
-   { url: "https://www.konjoko.com/blog/vardaat-az-dubai", lastModified: new Date() },
-   { url: "https://www.konjoko.com/services/shopping", lastModified: new Date() },
-   { url: "https://www.konjoko.com/services/payment", lastModified: new Date() },
-   { url: "https://www.konjoko.com/services/business", lastModified: new Date() },
-   { url: "https://www.konjoko.com/contact", lastModified: new Date() },
- ];
+  return [
+    { url: "https://www.konjoko.com", lastModified: new Date() },
+    { url: "https://www.konjoko.com/blog", lastModified: new Date() },
+    { url: "https://www.konjoko.com/blog/tajrobe-tajer-irani", lastModified: new Date() },
+    { url: "https://www.konjoko.com/blog/kharid-omde-dubai-chin", lastModified: new Date() },
+    { url: "https://www.konjoko.com/blog/kharid-az-dubai", lastModified: new Date() },
+    { url: "https://www.konjoko.com/blog/pardakht-beinalmelli", lastModified: new Date() },
+    { url: "https://www.konjoko.com/blog/kharid-az-chin", lastModified: new Date() },
+    { url: "https://www.konjoko.com/blog/kharid-az-turkey", lastModified: new Date() },
+    { url: "https://www.konjoko.com/blog/havaleh-arzi", lastModified: new Date() },
+    { url: "https://www.konjoko.com/blog/kharid-az-amazon", lastModified: new Date() },
+    { url: "https://www.konjoko.com/blog/vardaat-az-dubai", lastModified: new Date() },
+    { url: "https://www.konjoko.com/services/shopping", lastModified: new Date() },
+    { url: "https://www.konjoko.com/services/payment", lastModified: new Date() },
+    { url: "https://www.konjoko.com/services/business", lastModified: new Date() },
+    { url: "https://www.konjoko.com/contact", lastModified: new Date() },
+  ];
 }
